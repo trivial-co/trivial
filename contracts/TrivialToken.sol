69500c5ec68bdf7e4c992663c615d5ca413bbbf0
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-07-14 09:32:42 +0200

    First auction.

--- contracts/TrivialToken.sol
+++ contracts/TrivialToken.sol
@@ -6,30 +6,53 @@ contract TrivialToken is ERC223Token {
   uint256 public tokensForIco;
   uint256 public tokensForArtist;
   uint256 public tokensForTrivial;
-  uint256 public IcoGoal;
+  uint256 public icoGoal;
+
+  event FirstAuctionStarted();
+  event FirstAuctionFinished(amountRaised);
 
   enum State {Created, FirstAuctionStarted, FirstAuctionFinished};
   State currentState;
-  modifier onlyInState(GameState expectedState) {
-    require(expectedState == currentState)
-    _;
+
+  mapping(address => uint) public contributors;
+
+  modifier onlyInState(State expectedState) {
+    require(expectedState == currentState); _;
   }
 
   function TrivialToken(uint256 _icoGoal) {
-    currentState = State.Created;
     name = 'Trivial';
     symbol = 'TRVL';
     decimals = 3;
     totalSupply = 1000;
-    balances[0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5] = 100;
-    balances[0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0] = 100;
+    balances[0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5] = 111;
+    balances[0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0] = 222;
+
+    icoGoal = _icoGoal;
+    currentState = State.Created;
   }
 
   function startFirstAuction() onlyInState(State.Created) {
-
+    currentState = State.FirstAuctionStarted;
+    FirstAuctionStarted(goal);
   }
 
-  function contributeInIco() onlyInState(State.FirstAuctionStarted) {
-    
+  function contributeInIco() payable onlyInState(State.FirstAuctionStarted) {
+    require(msg.value);
+
+    contributors[msg.sender] += msg.value;
+    amountRaised += msg.value;
+
+    if (amountRaised >= icoGoal) {
+      currentState = State.FirstAuctionFinished;
+      FirstAuctionFinished(amountRaised);
+    }
+
+    /*
+      Reward contributor with amount of tokens equal to amount of sended eth.
+      Called at the end to ensure that gas error will not break it.
+      TODO: Could we use send here instead of balances?
+    */
+    balances[msg.sender] += msg.value;
   }
 }
