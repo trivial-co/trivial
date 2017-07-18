7e88a43c3a4263854673a2192ec75a1128eb3fc1
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-07-18 15:20:37 +0200

    Pass end time of Ico.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var TrivialToken = artifacts.require("./TrivialToken.sol");
 module.exports = function(deployer) {
   deployer.deploy(
       TrivialToken,
-      600,
+      Math.floor(Date.now() / 1000 + 600),
       600,
       '0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5',
       '0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0',
