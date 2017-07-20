8cf77f4cfde94a067899f90cd18f1ca164d58a1c
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-07-20 11:29:52 +0200

    Transaction instead of call.

--- contracts/DevelopmentTrivialToken.sol
+++ contracts/DevelopmentTrivialToken.sol
@@ -25,12 +25,17 @@ contract DevelopmentTrivialToken is TrivialToken {
     function setStateIcoFinished() { currentState = State.IcoFinished; }
     function setStateAuctionStarted() { currentState = State.AuctionStarted; }
     function setStateAuctionFinished() { currentState = State.AuctionFinished; }
-    function setTrivial() { trivial = msg.sender; }
-    function setArtist() { artist = msg.sender; }
+    function setTrivial(address sender) { trivial = sender; }
+    function becomeTrivial() { trivial = msg.sender; }
+    function setArtist(address sender) { artist = sender; }
+    function becomeArtist() { artist = msg.sender; }
     function setIcoEndTime(uint256 time) { icoEndTime = time; }
     function setIcoEndTimeTenMinutes() { icoEndTime = now + 10 minutes; }
     function setAuctionEndTime(uint256 time) { auctionEndTime = time; }
     function setAuctionEndTimeTenMinutes() { auctionEndTime = now + 10 minutes; }
     function becomeKeyHolder() {
         balances[msg.sender] = safeDiv(tokensForIco, TOKENS_PERCENTAGE_FOR_KEY_HOLDER); }
+    function getTrivial() returns (address) { return trivial; }
+    function getArtist() returns (address) { return artist; }
+    function getSelf() returns (address) { return msg.sender; }
 }
