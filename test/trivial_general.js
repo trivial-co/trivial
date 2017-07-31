049a207a34fd3e1cd9abc42869495fbc2339fe3b
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-07-31 09:21:10 +0200

    Use testrpc accounts.

--- test/trivial_general.js
+++ test/trivial_general.js
@@ -11,8 +11,8 @@ contract('TrivialToken - General tests', (accounts) => {
             'TRVLTEST',
             Math.floor(Date.now() / 1000 + 600),
             600,
-            '0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5',
-            '0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0',
+            accounts[8],
+            accounts[9],
             200000,
             100000,
             700000
