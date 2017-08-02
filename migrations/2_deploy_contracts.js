256120b92017794056062a1d7e5f6648b5e33ed1
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-08-02 16:31:28 +0200

    Use test accounts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,8 +8,8 @@ module.exports = function(deployer, network) {
             'TRVLTEST',
             Math.floor(Date.now() / 1000 + 600),
             600,
-            '0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5',
-            '0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0',
+            '0x71544d4D42dAAb49D9F634940d3164be25ba03Cc',
+            '0x71544d4D42dAAb49D9F634940d3164be25ba03Cc',
             200000,
             100000,
             700000
