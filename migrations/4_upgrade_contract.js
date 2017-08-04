58704e0d2be42871e1ccfecea07f0ca2950c06f9
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-08-04 16:19:26 +0200

    Item hash.

--- migrations/4_upgrade_contract.js
+++ migrations/4_upgrade_contract.js
@@ -7,7 +7,7 @@ module.exports = function(deployer, network) {
             '0xeAD3d0eD2685Bd669fe1D6BfdFe6F681912326D0', //address of deployed token
             'TrivialDev',
             'TRVLDEV',
-            '0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5', //hash of item description
+            'e6bd14970ce33d3f1abc794c8e3c7321da77e67a9790257f9a02838a75f9d4a65f073c4ab11fd2930919f11bd6f92abdd571e85d8d35a9711d12fde52b19e2fc', //hash of item description
             3 //contributorsCount
         );
     } else {
@@ -16,7 +16,7 @@ module.exports = function(deployer, network) {
             '0x158a96da5dcd1ade84ac11ac952683c0ee3516ab',
             'TrivialDev',
             'TRVLDEV',
-            '0xE5f25b81b38D29A6e9C4E6Bd755d09ea4Ed10ff5', //hash of item description
+            'e6bd14970ce33d3f1abc794c8e3c7321da77e67a9790257f9a02838a75f9d4a65f073c4ab11fd2930919f11bd6f92abdd571e85d8d35a9711d12fde52b19e2fc', //hash of item description
             7 //contributorsCount
         );
     }
